c2bf504737150c31cd1428b53c58849c44ebc52e
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-23 05:02:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word (string)
-  array = ("Hello there, and how are you?")
+  array = ("Hello there, and how are you?").split
   array.each 
 end 
 
 def reverse_each_word (string)
-  array = ("Hello there, and how are you?")
+  array = ("Hello there, and how are you?").split
   array.collect 
   
 end 
